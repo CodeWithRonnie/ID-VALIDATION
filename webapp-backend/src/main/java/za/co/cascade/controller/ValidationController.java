00308ffffe7afe6d70e1a114ffc660fe7e6ec0aa
Mamikie Maemu <maemu.ronewa98@gmai.com>
2025-04-23 14:03:58 +0200

    fix: Clean up and correct CORS configuration for all common local dev origins. Ready for frontend-backend integration.

--- webapp-backend/src/main/java/za/co/cascade/controller/ValidationController.java
+++ webapp-backend/src/main/java/za/co/cascade/controller/ValidationController.java
@@ -5,7 +5,7 @@ import za.co.cascade.model.ValidationRequest;
 import za.co.cascade.model.ValidationResponse;
 import za.co.cascade.ValidateSaIdWeb;
 
-@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:53409"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8080", "http://127.0.0.1:53409"})
 @RestController
 @RequestMapping("/api/validate")
 public class ValidationController {
